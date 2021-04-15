d0fa6853807951cd32daada86a18cf7c008df54c
Joshua Michalik <joshuasmichalik@gmail.com>
2021-04-15 07:18:39 -0400

    Create first version of Obsidian+

--- README.md
+++ README.md
@@ -1,56 +1,25 @@
-## Obsidian Sample Plugin
+## Obsidian+ Plugin
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+### Features
+**File Path**
+Reveals the path for the currently active file in the status bar.
+* Can be enabled/disabled in the settings
 
-This project uses Typescript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in Typescript Definition format, which contains TSDoc comments describing what it does.
+**Possible Links**
+Searches for all notes in the vault that whose title includes a word in the active note.
+* Ignoring all words with a specified max-number of characters in settings.
+* Ignore journal path
+* Ignoring words (e.g. and, but, they, etc) as specified in settings.
 
-**Note:** The Obsidian API is still in early alpha and is subject to change at any time!
+**Random Note**
+Opens a random note in the vault. 
+* Ribbon icon can be enabled/disabled in the settings.
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Changes the default font color to red using `styles.css`.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+**Reading Time**
+Displays an estimated reading time in minutes in the status bar.
+* Can be enabled/disabled in the settings.
+* Words Per Minute (WPM) reading speed can be changed in the settings.
 
-### First time developing plugins?
-
-Quick starting guide for new plugin devs:
-
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-
-### Releasing new releases
-
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments.
-- Publish the release.
-
-### Adding your plugin to the community plugin list
-
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
-
-### How to use
-
-- Clone this repo.
-- `npm i` or `yarn` to install dependencies
-- `npm run dev` to start compilation in watch mode.
-
-### Manually installing the plugin
-
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
-
-### API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
+### Commands
+**Open Random File** (Default hotkey: <CMD>-8)
+Run this command to open a random file in the currently open vault.
--- main.ts
+++ main.ts
@@ -1,56 +1,144 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
 interface MyPluginSettings {
-	mySetting: string;
+	filterWords: string;
+	dailies: string;
+	minLetters: number;
+	showFilePath: boolean;
+	showRandom: boolean;
+	showReadingTime: boolean;
+	wpm: number;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	filterWords: 'the,and,but,not,then,they,will,not,your,from,them,was,with,what,who,why,where,this,over,than',
+	dailies: '',
+	minLetters: 3,
+	showFilePath: true,
+	showRandom: true,
+	showReadingTime: true,
+	wpm: 200,
 }
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
-		console.log('loading plugin');
+		console.log('loading Obsidian+');
 
 		await this.loadSettings();
 
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
-		});
+		const FILE_PATH_STATUS = this.addStatusBarItem();
+		const READING_TIME_STATUS = this.addStatusBarItem();
 
-		this.addStatusBarItem().setText('Status Bar Text');
+		const updateFilePath = () => {
+			let activeFile = this.app.workspace.getActiveFile();
+
+			if (this.settings.showFilePath) {
+				FILE_PATH_STATUS.setText(activeFile.path);
+			} else {
+				FILE_PATH_STATUS.setText('');
+			}
+		}
+
+		const updateReadingTime = async () => {
+			let activeFile = this.app.workspace.getActiveFile();
+			let fileData = await this.app.vault.read(activeFile);
+
+			let result = "0m";
+			let textLength = fileData ? fileData.split(" ").length : 0;
+			if (textLength > 0) {
+				let value = Math.ceil(textLength / this.settings.wpm);
+				result = `${value}m`;
+			}
+
+			if (this.settings.showReadingTime) {
+				READING_TIME_STATUS.setText(result);
+			} else {
+				READING_TIME_STATUS.setText('');
+			}
+		}
+
+		const openRandomNote = () => {
+			let files = this.app.vault.getFiles();
+			let random = Math.floor(Math.random() * files.length);
+			let randomFile = files[random];
+
+			const currentLeaf = this.app.workspace.activeLeaf;
+			currentLeaf.openFile(randomFile);
+		}
+
+		const getPossibleLinks = async () => {
+			let files = this.app.vault.getFiles();
+			let activeFile = this.app.workspace.getActiveFile();
+			let fileData = await this.app.vault.read(activeFile);
+			fileData = fileData ? fileData : "";
+
+			const currentView = this.app.workspace.activeLeaf.view;
+			const cm = (currentView as any).sourceMode.cmEditor
+			const cursor = cm.getCursor()
+			const selectedRange = cm.getSelections().join('\n')
+
+			fileData = selectedRange || fileData.replace(/\W+/g," ");
+			let links: any[] = [];
+			let fileTextItems = fileData.split(" ");
+			fileTextItems = [...new Set(fileTextItems)];
+			fileTextItems = fileTextItems.map(text => text.replace(/\s+/g, ""))
+							 					  .filter(t => t.length > this.settings.minLetters && this.settings.filterWords.split(",").indexOf(t.toLowerCase()) == -1);
+			let keywords: any = {};
+			fileTextItems.forEach(text => {
+				text = text.toLowerCase();
+				files.forEach(file => {
+					if ((this.settings.dailies == '' || (this.settings.dailies != '' && file.path.indexOf(this.settings.dailies) == -1)) 
+					&& file.extension == "md"
+					&& file.basename.toLowerCase().indexOf(text) > -1
+					) {
+						if (keywords[text] === undefined) {
+							keywords[text] = [];
+						}
+						keywords[text].push(file);
+					}
+				});
+			});
+			new KeywordsModal(this.app, keywords).open();
+		}
+
+		this.app.workspace.on('active-leaf-change', updateFilePath);
+		this.app.workspace.on('file-open', updateFilePath);
+		this.app.workspace.on('click', updateFilePath);
+		this.app.workspace.on('active-leaf-change', updateReadingTime);
+		this.app.workspace.on('file-open', updateReadingTime);
+		this.app.workspace.on('click', updateReadingTime);
 
 		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-					return true;
+			id: 'obp-open-random-note',
+			name: 'Open Random Note',
+			callback: openRandomNote,
+			hotkeys: [
+				{
+					modifiers: ["Mod"],
+					key: "8"
 				}
-				return false;
-			}
+			]
 		});
 
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addCommand({
+			id: 'obp-show-possible-links',
+			name: 'Show Possible Links',
+			callback: getPossibleLinks,
+			hotkeys: [
+				{
+					modifiers: ["Mod"],
+					key: "6"
+				}
+			]
+		})
 
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		});
+		if (this.settings.showRandom) {
+			this.addRibbonIcon('dice', 'Open Random Note', openRandomNote);
+		}
 
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new ObsidianPlusSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -66,14 +154,32 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class KeywordsModal extends Modal {
+	constructor(app: App, public keywords: any) {
 		super(app);
 	}
 
 	onOpen() {
 		let {contentEl} = this;
-		contentEl.setText('Woah!');
+		let section = document.createElement("div");
+		section.addClass('possible-links-container');
+		let keys = Object.keys(this.keywords);
+		let title = document.createElement("h3");
+		title.setText(`${keys.length} keywords found`);
+		section.append(title);
+
+		keys.sort((a,b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+		keys.map(keyword => {
+			let title = document.createElement("p");
+			title.addClass('possible-link-item');
+			title.setText(`${keyword} - ${this.keywords[keyword].length} notes found`);
+			title.addEventListener('click', () => {
+				new PossibleLinksModal(this.app, this.keywords[keyword], this.keywords).open();
+				this.close();
+			});
+			section.append(title);
+		});
+		contentEl.append(section);
 	}
 
 	onClose() {
@@ -82,7 +188,62 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
+class PossibleLinksModal extends Modal {
+	constructor(app: App, public links: any, public keywords: any) {
+		super(app);
+	}
+
+	onOpen() {
+		let {contentEl} = this;
+		let section = document.createElement("div");
+		section.addClass('possible-links-container');
+		let back = document.createElement("h4");
+		back.setText(`< Back to Keywords`);
+		back.addClass('possible-link-item');
+		back.addEventListener('click', () => {
+			new KeywordsModal(this.app, this.keywords).open();
+			this.close();
+		});
+		contentEl.append(back);
+		let title = document.createElement("h3");
+		title.setText(`${this.links.length} notes found`);
+		section.append(title);
+		this.links.map((link: any) => {
+			let aref = document.createElement("a");
+			aref.setText(link.path);
+			aref.addClass('possible-link-item');
+			aref.addEventListener('click', async (e) => {
+				const currentLeaf = this.app.workspace.activeLeaf;
+				if (e.metaKey) {
+					let newLeaf = this.app.workspace.splitActiveLeaf('vertical');
+					newLeaf.openFile(link);
+				} else {
+					currentLeaf.openFile(link);
+				}
+				let activeFile = this.app.workspace.getActiveFile();
+				let fileData = await this.app.vault.read(activeFile);
+				this.app.vault.modify(activeFile, fileData + `\n[[${link.basename}]]`);
+				new Notice(`Added link [[${link.basename}]] to end of '${activeFile.basename}'`)
+				this.close();
+			});
+			let p = document.createElement("p");
+			p.append(aref);
+			section.append(p);
+		});
+		contentEl.append(section);
+
+		if (this.links.length == 0) {
+			contentEl.setText("0 Notes Found");
+		}
+	}
+
+	onClose() {
+		let {contentEl} = this;
+		contentEl.empty();
+	}
+}
+
+class ObsidianPlusSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -92,21 +253,107 @@ class SampleSettingTab extends PluginSettingTab {
 
 	display(): void {
 		let {containerEl} = this;
-
 		containerEl.empty();
+		containerEl.createEl('h2', {text: 'Settings for Obsidian+'});
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
+		// File Path
+		containerEl.createEl('h3', {text: 'File Path'});
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
+			.setName('Display File Path')
+			.setDesc('Show the active file path in the status bar.')
+			.addToggle(toggle => toggle.setValue(this.plugin.settings.showFilePath)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.showFilePath = value;
 					await this.plugin.saveSettings();
 				}));
+
+
+		// Possible Links
+		containerEl.createEl('h3', {text: 'Possible Links'});
+		new Setting(containerEl)
+			.setName('Minimum Letters')
+			.setDesc('Minimum letter count for a word when searching for related notes.')
+			.addText(text => text
+				.setPlaceholder('3')
+				.setValue(this.plugin.settings.minLetters.toString())
+				.onChange(async (value) => {
+					this.plugin.settings.minLetters = parseInt(value);
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Ignore Dailies Path')
+			.setDesc('Specify folder of Daily Journal to ignore these notes when searching for possible links. (leave blank to include dailies in possible links)')
+			.addText(text => text
+				.setPlaceholder('dailies')
+				.setValue(this.plugin.settings.dailies)
+				.onChange(async (value) => {
+					this.plugin.settings.dailies = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Filtered Words')
+			.setDesc('Words filtered when searching for related notes. (separated by comma, no spaces)')
+			.addTextArea(text => {
+				text
+					.setPlaceholder('and,but,they...')
+					.setValue(this.plugin.settings.filterWords)
+					.onChange(async (value) => {
+						this.plugin.settings.filterWords = value;
+						await this.plugin.saveSettings();
+					});
+				text.inputEl.rows = 10;
+				text.inputEl.cols = 25;
+			});
+
+		// new Setting(containerEl)
+		// 	.setName('Include paths')
+		// 	.setDesc('Paths in vault to search for possible links. (leave blank to search entire vault')
+		// 	.addTextArea(text => {
+		// 		text
+		// 			.setPlaceholder('root/to/folder')
+		// 			.setValue(this.plugin.settings.dailies)
+		// 			.onChange(async (value) => {
+		// 				this.plugin.settings.dailies = value;
+		// 				await this.plugin.saveSettings();
+		// 			});
+		// 		text.inputEl.rows = 10;
+		// 		text.inputEl.cols = 25;
+		// 	});
+
+		// Random Note
+		containerEl.createEl('h3', {text: 'Random Note'});
+		new Setting(containerEl)
+			.setName('Display Random Icon in Ribbon. (Requires reload)')
+			.setDesc('Show the random file option in the ribbon.')
+			.addToggle(toggle => toggle.setValue(this.plugin.settings.showRandom)
+				.onChange(async (value) => {
+					this.plugin.settings.showRandom = value;
+					await this.plugin.saveSettings();
+				}));
+
+		// Reading Time
+		containerEl.createEl('h3', {text: 'Reading Time'});
+		new Setting(containerEl)
+			.setName('Display Reading Time')
+			.setDesc('Show the estimated reading time in the status bar.')
+			.addToggle(toggle => toggle.setValue(this.plugin.settings.showReadingTime)
+				.onChange(async (value) => {
+					this.plugin.settings.showReadingTime = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Words Per Minute')
+			.setDesc('Words per minute calculated and added to status bar.')
+			.addText(text => text
+				.setPlaceholder('200')
+				.setValue(this.plugin.settings.wpm.toString())
+				.onChange(async (value) => {
+					this.plugin.settings.wpm = parseInt(value);
+					await this.plugin.saveSettings();
+				}));
+
 	}
 }
